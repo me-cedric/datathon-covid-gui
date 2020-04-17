b40a8585ff8d3751bb6439ad53171c4263e5e3cf
Cédric Meyer <mecedricod@gmail.com>
2020-04-18 01:18:15 +0200

    setup front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core'
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { MatStep, MatStepper } from '@angular/material/stepper'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'datathon-covid-gui-frontend';
+  // Steppers
+  @ViewChild('stepper') stepper: MatStepper
+  @ViewChild('imageStep') imageStep: MatStep
+  @ViewChild('resultStep') resultStep: MatStep
+
+  // form groupo to valdiate presence of file
+  imageFormGroup = new FormGroup({
+    files: new FormControl('', Validators.required)
+  })
+  // List of file
+  files: File[] = []
+
+  /**
+   * Add a file
+   * event: { addedFiles: any }
+   */
+  onSelect(event: { addedFiles: any }) {
+    this.files.push(...event.addedFiles)
+    this.validate()
+  }
+
+  /**
+   * Remove a file
+   * event: File
+   */
+  onRemove(event: File) {
+    this.files.splice(this.files.indexOf(event), 1)
+    this.validate()
+  }
+
+  /**
+   * Validate presence of file
+   */
+  validate() {
+    if (this.files.length > 0) {
+      this.imageFormGroup.setValue({
+        files: true
+      })
+    } else {
+      this.imageFormGroup.setValue({
+        files: null
+      })
+    }
+  }
+
+  /**
+   * Reset the images
+   */
+  reset() {
+    this.files = []
+    this.stepper.reset()
+  }
+
+  isFileImage = (file: File) => (file && file.type.split('/')[0] === 'image')
+
+  submit() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { HttpClientModule } from '@angular/common/http'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NgxDropzoneModule } from 'ngx-dropzone'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { MaterialModule } from './shared/material.module'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FlexLayoutModule,
+    MaterialModule,
+    NgxDropzoneModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
